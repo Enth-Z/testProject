7a7df05fca2f487fe8cb57376738a546f3372df8
Enth-Z <enth20bb@gmail.com>
2021-03-19 12:00:48 +0800

    3.19test

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,5 @@
 print('hello wprld')
 print(444)
 print('w')
-sdasd vscode://vscode.github-authentication/did-authenticate?windowid=1&code=1211c801ad11844889ea&state=da157852-229a-49b6-a0b6-8348f164e13f
+
+print('更改') 
